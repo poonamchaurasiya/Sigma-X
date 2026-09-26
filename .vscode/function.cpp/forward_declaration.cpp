d9ff0658c21ab7aad3074dcc7f2f096b1548119c
Punam Chaurasia <181643325+poonamchaurasiya@users.noreply.github.com>
2026-09-26 21:32:38 +0530

    forward declaration

---
+++ .vscode/function.cpp/forward_declaration.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a,int b){
+    int sum = a+b;
+    return sum;
+}
+int main(){
+   int s =sum(2,4);
+   cout <<"sum="<< s<<endl;
+    return 0;
+}
